d8808f250feaacda8e17f13fda9c0c6f75be1ab2
Alumno <Alumno@EQ21-LAB1>
2014-04-09 19:49:23 -0300

    agregar label

--- lab02b/src/lab02b/ventana.java
+++ lab02b/src/lab02b/ventana.java
@@ -1,11 +1,15 @@
 package lab02b;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class ventana {
 	public static void main(String[] args)
 	{
 		JFrame frame = new JFrame();
+		JLabel label = new JLabel();
+		label.setText("Hola soy una Label");
+		frame.add(label);
 		frame.setTitle("Mi segunda ventana");
 		frame.setSize(800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
