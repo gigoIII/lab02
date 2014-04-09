256ceeb8bf8784e5822ea876283ac01b8fb34dec
Alumno <Alumno@EQ21-LAB1>
2014-04-09 19:38:10 -0300

    crear ventana

---
+++ lab02b/src/lab02b/ventana.java
@@ -0,0 +1,15 @@
+package lab02b;
+
+import javax.swing.JFrame;
+
+public class ventana {
+	public static void main(String[] args)
+	{
+		JFrame frame = new JFrame();
+		frame.setTitle("Mi primera ventana");
+		frame.setSize(800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+}
